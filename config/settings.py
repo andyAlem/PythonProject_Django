a88f80f5514bfc8ca8a126a140a809e7e57d3b44
Andrej <andrej@mail.com>
2025-04-15 20:23:53 +0200

    added import command, addedfixture

--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import os
 
+from django.conf.global_settings import MEDIA_URL
 from dotenv import load_dotenv
 
 load_dotenv(override=True)
@@ -96,3 +97,7 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = 'media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
