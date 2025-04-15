8d5a96ed30230eae746eec4878b0e2df710db028
Andrej <andrej@mail.com>
2025-04-15 21:04:24 +0200

    performed lack and isort

--- config/settings.py
+++ config/settings.py
@@ -58,7 +58,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2' ,
-        'NAME': os.getenv('django_project_1'),
+        'NAME': os.getenv('NAME'),
         'USER': os.getenv('USER'),
         'PASSWORD': os.getenv('PASSWORD'),
         'HOST': os.getenv('HOST'),
